af651553e23cd735de5f24bb6e7b26e02b6018f4
USER <USER@DESKTOP-EO932BE>
2022-01-06 23:53:36 +0900

    초기 커밋.

---
+++ Jinjoo_Ascii/Jinjoo_Ascii.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Jinjoo_Ascii
+{
+    class CharToAscii
+    {
+        static void Converter()
+        {
+            char input = Convert.ToChar(Console.ReadLine());
+            int output = Convert.ToInt32(input);
+            Console.WriteLine(output);
+        }
+
+        static void Main(string[] args)
+        {
+            Converter();
+        }
+    }
+}
